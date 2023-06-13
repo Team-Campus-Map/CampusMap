d9e0e43305b30e8d4be4ed5fa86ddf594f14c3d0
gxxu0 <94434730+gxxu0@users.noreply.github.com>
2023-06-13 23:55:57 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./Main/Main";
 import AuthPage from "./LoginSignUp/AuthPage";
-import FindPw from "./LoginSignUp/Login/FindPw";
 import Calendar from "./Calendar/Calendar";
 import Timetable from "./Timetable/Timetable";
 import Map from "./Map/Map";
@@ -16,7 +15,6 @@ function App() {
           <Route path="/" element={<Main />} />
           <Route path="/About" element={<About />} />
           <Route path="/login" element={<AuthPage />} />
-          <Route path="/FindPw" element={<FindPw />} />
           <Route path="/Calendar" element={<Calendar />} />
           <Route path="/Timetable" element={<Timetable />} />
           <Route path="/Map" element={<Map />} />
